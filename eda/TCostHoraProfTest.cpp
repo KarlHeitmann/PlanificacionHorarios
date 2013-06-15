fb2d8b875830418cd9c5dd4128f1d7fba5cae59a
KarlHeitmann <khf1988@gmail.com>
2013-06-15 09:43:38 -0400

    Buenas pruebas

--- eda/TCostHoraProfTest.cpp
+++ eda/TCostHoraProfTest.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 int main (int argc, char **argv) {
 	std::vector<TCostHoraProf> LCostHoraProf;
-	//TCostHoraProf CostHoraProf[argc];
+	const char *pchrtemp[argc];
 	for (int i=0; i< argc; i++) {
 		LCostHoraProf.push_back( TCostHoraProf(argv[i], i) );
 	}
 	for (unsigned i=1; i< argc; i++) {
 		//cout << "Hola";
+		pchrtemp[i]=LCostHoraProf[i].GetHorario();
 		cout << i << " Horario: " << LCostHoraProf[i].GetHorario() << "\tCoste: " << LCostHoraProf[i].GetCoste() << "\n";
 	}	
+	for (unsigned i=1; i< (argc-0); i++)
+		cout << i <<" Horario: " << pchrtemp[i] << "\n";
 }
 
