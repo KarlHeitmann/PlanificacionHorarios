5391c7d710c9d1db3e67d4244b9f92602b2a463f
KarlHeitmann <khf1988@gmail.com>
2013-06-15 00:17:26 -0400

    Primera compilacion

---
+++ eda/TCostHoraProf.cpp
@@ -0,0 +1,8 @@
+#include "TCostHoraProf.h"
+
+TCostHoraProf :: TCostHoraProf (const char *_pchrHorario, int _intCoste) {
+	this->pchrHorario=_pchrHorario;
+	this->intCoste=_intCoste;
+}
+
+
---
+++ eda/TCostHoraProf.h
@@ -0,0 +1,17 @@
+/* 
+ * Es el objeto que relaciona la preferencia del profesor por los diferentes
+ * horarios
+ *
+ * */
+#ifndef T_COST_HORA_PROF_H
+#define T_COST_HORA_PROF_H
+
+class TCostHoraProf {
+	private:
+		const char *pchrHorario;
+		int intCoste;
+	public:
+		TCostHoraProf(const char*, int);
+};
+
+#endif
