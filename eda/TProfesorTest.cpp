5565ef48782330754a418b859dbd840b08ac9864
KarlHeitmann <khf1988@gmail.com>
2013-06-16 15:04:59 -0400

    Test running OK

--- eda/TProfesorTest.cpp
+++ eda/TProfesorTest.cpp
@@ -22,6 +22,18 @@ int main (int argc, char **argv) {
 	pCostAsigProf->GenAddCostHoraProf("V2", 1);
 	pCostAsigProf->GenAddCostHoraProf("V3", 0);
 	Profesor.PutCostAsigProf(*pCostAsigProf);
+	
+	pCostAsigProf=new TCostAsigProf(79);	
+	pCostAsigProf->GenAddCostHoraProf("L1", 2);
+	pCostAsigProf->GenAddCostHoraProf("L2", 1);
+	pCostAsigProf->GenAddCostHoraProf("L3", 0);
+	pCostAsigProf->GenAddCostHoraProf("M1", 2);
+	pCostAsigProf->GenAddCostHoraProf("M2", 0);
+	pCostAsigProf->GenAddCostHoraProf("M3", 1);
+	pCostAsigProf->GenAddCostHoraProf("S1", 2);
+	pCostAsigProf->GenAddCostHoraProf("S2", 1);
+	pCostAsigProf->GenAddCostHoraProf("S3", 0);
+	Profesor.PutCostAsigProf(*pCostAsigProf);
 
 	strTemp=Profesor.GetSummary();
 	
