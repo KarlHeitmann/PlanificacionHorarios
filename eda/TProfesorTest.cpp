bd1315daa722543e4552f9940900d49f2986e3f1
KarlHeitmann <khf1988@gmail.com>
2013-06-16 14:32:52 -0400

    Pasando a punteros

--- eda/TProfesorTest.cpp
+++ eda/TProfesorTest.cpp
@@ -10,17 +10,18 @@ int main (int argc, char **argv) {
 	
 	std::string strTemp="";
 
-	TCostAsigProf CostAsigProf(69);	
-	CostAsigProf.GenAddCostHoraProf("M1", 2);
-	CostAsigProf.GenAddCostHoraProf("M2", 1);
-	CostAsigProf.GenAddCostHoraProf("M3", 0);
-	CostAsigProf.GenAddCostHoraProf("J1", 2);
-	CostAsigProf.GenAddCostHoraProf("J2", 0);
-	CostAsigProf.GenAddCostHoraProf("J3", 1);
-	CostAsigProf.GenAddCostHoraProf("V1", 2);
-	CostAsigProf.GenAddCostHoraProf("V2", 1);
-	CostAsigProf.GenAddCostHoraProf("V3", 0);
-	Profesor.PutCostAsigProf(CostAsigProf);
+	TCostAsigProf *pCostAsigProf;
+	pCostAsigProf=new TCostAsigProf(69);	
+	pCostAsigProf->GenAddCostHoraProf("M1", 2);
+	pCostAsigProf->GenAddCostHoraProf("M2", 1);
+	pCostAsigProf->GenAddCostHoraProf("M3", 0);
+	pCostAsigProf->GenAddCostHoraProf("J1", 2);
+	pCostAsigProf->GenAddCostHoraProf("J2", 0);
+	pCostAsigProf->GenAddCostHoraProf("J3", 1);
+	pCostAsigProf->GenAddCostHoraProf("V1", 2);
+	pCostAsigProf->GenAddCostHoraProf("V2", 1);
+	pCostAsigProf->GenAddCostHoraProf("V3", 0);
+	Profesor.PutCostAsigProf(*pCostAsigProf);
 
 	strTemp=Profesor.GetSummary();
 	
